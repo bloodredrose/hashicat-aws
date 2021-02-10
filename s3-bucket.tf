57458391c2cbe43d35f916238a7761c9a347b53e
Barbara Shaurette <bshaurette@outlook.com>
2021-02-10 19:30:26 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -2,7 +2,5 @@ module "s3-bucket" {
   source  = "app.terraform.io/bshaurette-training/s3-bucket/aws"
   version = "1.17.0"
   # insert required variables here
-  inputs = {
-    bucket_prefix = "barbara-shaurette"
-  }
+  bucket_prefix = "barbara-shaurette"
 }
